527bd3bf11426fc3914ae7c9924400304a20f91c
William <Williamtviet@hotmail.com>
2023-02-17 05:18:03 -0800

    fixed timer for negative numbers

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
-console.log(process.argv[2]);
-const timer = function() {
+//console.log(process.argv[2]);
+const timer = function(processArgv) { //not sure what to name value inside function
   let input = process.argv;
   if (input === undefined) {
     console.log(undefined);
@@ -8,12 +8,14 @@ const timer = function() {
   //console.log (input)
   setTimeout(() => {
     process.stdout.write('\x07'); //sound
-  }, input * 1000);
+  }, processArgv * 1000);
 };
 
 for (let i = 2; i < process.argv.length; i ++) { //start at 2 because of wierd process.argv
-  timer(process.argv[i]);
-
+  if ((process.argv[i]) < 0) {    
+  } else {
+    timer(process.argv[i]); //i should study on callbacks more
+  }
 }
 
 // timer();
